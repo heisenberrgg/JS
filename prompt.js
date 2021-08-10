431085fe4c808b133beac29fce36684891b82f71
heisenberrgg <85861152+heisenberrgg@users.noreply.github.com>
2021-08-10 15:48:33 +0530

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function(){
+        const name= document.querySelector('#name').value;
+        alert(`Hello, ${name}!`);
+    };
